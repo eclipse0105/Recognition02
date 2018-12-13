4d739a168cb4207c13dc90d86b53c2f63bd86f7e
SakaiRyo <SakaiRyo@172.16.210.49>
2018-12-13 16:10:34 +0900

    2018/12/13課題２−２

--- src/recognition02/Recognition02_main.java
+++ src/recognition02/Recognition02_main.java
@@ -9,31 +9,17 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
 
+import recognition02.Recognition02_lib;
+
 public class Recognition02_main {
 
 	public static void main(String[] args) 
 	{
-		VisualRecognition service = new VisualRecognition("2018-03-19");
-		IamOptions iamOptions = new IamOptions.Builder()
-		  .apiKey("===")
-		  .build();
-		service.setIamCredentials(iamOptions);
+		Recognition02_lib rlib = new Recognition02_lib();
+		rlib.getResult("img/nashi.JPG");
+		 
 
-		ClassifyOptions classifyOptions = null;
-		try {
-			classifyOptions = new ClassifyOptions.Builder()
-					  .imagesFile(new File("img/nashi.jpg"))
-					  .imagesFilename("nashi.jpg")
-					  .threshold((float) 0.6)
-					  .owners(Arrays.asList("IBM"))
-					  .build();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		ClassifiedImages result = service.classify(classifyOptions).execute();
-	    System.out.println(result);
+		
 
 
 	}
